37bdd93ebd10673189c408d56ad786bbea4d1df7
Mahesh Kakde <maheshkakde100@gmail.com>
2020-12-04 12:38:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello world');
+    res.send('Hey Its working fine');
 });
 
-app.get('/alien', function (req, res){
+app.get('/patch', function (req, res){
     const id = req.query.id;
-    res.send('Welcome back Alien'+id);
+    res.send('Welcome back patch');
 });
 
-app.get('/alien/:id', function (req, res) {
+app.get('/patch/:id', function (req, res) {
     const id = req.params.id;
     res.send('Hey Mahesh' +id);
 });
 
 app.listen(9000, function (req, res) {
-    console.log('Running');
+    console.log('Server Is Up and Running');
 });
